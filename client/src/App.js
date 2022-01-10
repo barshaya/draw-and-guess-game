cb709cc589fa6d4d0ecb0b4a8c8c49503f8bcce7
barshaya <barshaya97@gmail.com>
2022-01-10 22:12:31 +0200

    change the home style

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,21 @@
 import "./App.css";
 import { useState } from "react";
-//import Header from "./components/Header/Header";
 import Login from "./pages/Login/Login";
-import Room from "./components/Room/Room";
-import Rooms from './components/Rooms/Rooms';
+import Home from "./pages/Home";
 
 function App() {
   const [userName, setUserName] = useState("");
-  const [rooms, setRooms] = useState([
-    {
-      id: 1,
-      name: "Bar's Room",
-      numOfMembers: 2,
-    },
-    {
-      id: 2,
-      name: "Amnon's Room",
-      numOfMembers: 1,
-    },
-    {
-      id: 3,
-      name: "The Drawers",
-      numOfMembers: 0,
-    },
-  ]);
-
+  
   const onAddName = (name) => {
     setUserName(name);
-  };
-
-  const toggle= (id)=>{
-    console.log(id);
   };
 
   return (
     <div className="App">
       {userName === "" ? (
         <Login onAddName={onAddName}></Login>
-      ) : (
-        //<Header name={userName}></Header>
-        Rooms.length > 0 ? 
-          <Rooms rooms={rooms} 
-          onToggle={toggle}/> : 'No Rooms To Show'
-      )}
+      ) : <Home userName={userName}></Home>
+      }
     </div>
   );
 }
