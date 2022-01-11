e45d04aa9424df3c88ccefe01b7fa55b3bb712b6
barshaya <barshaya97@gmail.com>
2022-01-12 01:12:17 +0200

    add Canvas functionallity

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,13 +1,59 @@
 import { useState , useRef } from "react";
 import CanvasDraw from "react-canvas-draw";
-import './Canvas.css';
 
 const Canvas = () => {
+    const [color, setColor] = useState("#000");
+    const [brushRadius, setBrushRadius] = useState(2);
+
+    const canvasRef = useRef();
+    let canvasObject;
+
+    const changeColor=()=>{
+        setColor("#139")
+    }
+
+    const changeRadius=()=>{
+        setBrushRadius(10)
+    }
+
+    const remove=()=>{
+        setColor("#ffff")
+    }
+
+    const eraseAll=()=>{
+        canvasObject = canvasRef.current;
+        canvasObject.eraseAll();
+    }
+
+    const undo=()=>{
+        canvasObject = canvasRef.current;
+        canvasObject.undo();
+    }
+
+    let string;
+    const sendImg=()=>{
+        canvasObject = canvasRef.current;
+        string = canvasObject.getSaveData();
+        console.log(string);
+    }
+    const getImg=()=>{
+        canvasObject = canvasRef.current;
+        canvasObject.loadSaveData(string);
+    }
 
     return (
         
-        <div >
-            <CanvasDraw />
+        <div>
+            <div>
+            <div onClick={changeColor}>color</div>
+            <di vonClick={changeRadius}>radius</di>
+            <div onClick={remove}>remove</div>
+            <div onClick={eraseAll}>reset</div>
+            <div onClick={undo}>undo</div>
+            <div onClick={sendImg}>send</div>
+            <div onClick={getImg}>get</div>
+            </div>
+            <CanvasDraw  brushColor={color}  brushRadius={brushRadius}  lazyRadius={1} hideGrid={false} canvasWidth={310} canvasHeight={500} ref={canvasRef}/>
         </div>
     )
 }
