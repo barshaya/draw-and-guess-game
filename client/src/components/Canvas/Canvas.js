69697b7e3ba3a9eb941a27df51f66aceda724358
barshaya <barshaya97@gmail.com>
2022-01-12 01:10:27 +0200

    import Canvas component

---
+++ client/src/components/Canvas/Canvas.js
@@ -0,0 +1,15 @@
+import { useState , useRef } from "react";
+import CanvasDraw from "react-canvas-draw";
+import './Canvas.css';
+
+const Canvas = () => {
+
+    return (
+        
+        <div >
+            <CanvasDraw />
+        </div>
+    )
+}
+
+export default Canvas
