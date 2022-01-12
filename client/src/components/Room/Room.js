f81517ba908f868547d976dee6d4b492d61be44f
barshaya <barshaya97@gmail.com>
2022-01-12 16:58:11 +0200

    add Room component style

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -3,13 +3,14 @@ import "./Room.css";
 
 const Room = ({ room, onToggle }) => {
   return (
-    <div
-      className={`room ${room.numOfMembers === 2 ? "full" : ""}`}
+    <button
+      className='room-item'
       onDoubleClick={()=>onToggle(room.id)}
+      disabled={room.numOfMembers === 2}
     >
       <h3>{room.name}</h3>
       <span>{room.numOfMembers}</span>
-    </div>
+    </button>
   );
 };
 
--- client/src/components/Rooms/Rooms.js
+++ client/src/components/Rooms/Rooms.js
@@ -1,12 +1,14 @@
 import Room from "../Room/Room";
 
+import './Rooms.css'
+
 const Tasks = ({ rooms, onToggle }) => {
   return (
-    <>
+    <div className="rooms-container">
       {rooms.map((room) => (
         <Room key={room.id} room={room} onToggle={onToggle} />
       ))}
-    </>
+    </div>
   );
 };
 
