65e58bd797b785537486c3e316ff1a50211e7295
barshaya <barshaya97@gmail.com>
2022-01-16 02:00:46 +0200

    server connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
+const cors= require('cors');
 const app = require('express')()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http,{
+  cors:{
+    origin:"http://localhost:3000",
+    methods:["GET","POST"]
+  }
+})
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+app.use(cors(corsOptions));
+
+let userName='';
+let players=[]
 
 io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
+  console.log('connected');
+  socket.on('userLogged', (userName) => {
+    players.push(userName)
+    console.log(userName,{players})
+    if(players.length===1){
+      socket.emit('setDrawing')
+    }
+    if(players.length===2){
+      socket.broadcast.emit('startGame')
+      socket.emit('startGame')
+    }
+    // io.emit('message', { name, message })
+  })
+
+  //handle drawing
+  socket.on('sentDrawing',(drawingVideo)=>{
+    socket.broadcast.emit('getDrawing',drawingVideo)
   })
 })
+
 
 http.listen(4000, function() {
   console.log('listening on port 4000')
